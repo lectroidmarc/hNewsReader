25be84c1649458509aa129d5c562c4a5a3b80f91
Marc Matteo <marcm@lectroid.net>
2011-09-02 20:30:04 -0700

    Remove Microformat-shiv.  It was overkill for what we need.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 hNews Reader
 ====
 
-A Chrome extension for reading the [hNews](http://microformats.org/wiki/hnews) microformat.  Primarily relies on [Microformat Shiv](http://microformatshiv.com/) for microformat handling.
+A Chrome extension for reading the [hNews](http://microformats.org/wiki/hnews) microformat.
 
 A fully packed extension for testing is located at [http://www.lectroid.net/chrome/hNewsReader.crx](http://www.lectroid.net/chrome/hNewsReader.crx)
 
--- hNewsReader/hnewsreader_content.js
+++ hNewsReader/hnewsreader_content.js
@@ -1,3 +1,92 @@
+/**
+  Container for an hNews object.  Based on the hNews 0.1 spec at http://microformats.org/wiki/hnews
+  @param hNewsElem a DOM element that is a hNews container.
+*/
+var hNews = function (hNewsElem) {
+	// entry-title, from hAtom, required
+	var entryTitles = hNewsElem.getElementsByClassName('entry-title');
+	if (entryTitles.length > 0) {
+		this.title = entryTitles[0].innerText.trim();
+	}
+
+	// author, from hAtom, required
+	var authors = hNewsElem.getElementsByClassName('author vcard');
+	if (authors.length > 0) {
+		var authorFNs = authors[0].getElementsByClassName('fn');
+		if (authorFNs.length > 0) {
+			this.author = authorFNs[0].innerText.trim();
+		}
+	}
+
+	// updated, from hAtom, required
+	var updatedDates = hNewsElem.getElementsByClassName('updated');
+	if (updatedDates.length > 0) {
+		this.updated = Date.parse(updatedDates[0].getAttribute('title'));
+	}
+
+	// source-org, from hNews, required
+	var sources = hNewsElem.getElementsByClassName('source-org vcard');
+	if (sources.length > 0) {
+		var sourceFNs = sources[0].getElementsByClassName('fn');
+		if (sourceFNs.length > 0) {
+			this.source = sourceFNs[0].innerText.trim();
+		}
+	}
+
+	// item-license, from hNews, recommended
+	var licenseSnapshots = document.evaluate("//a[@rel='item-license']", hNewsElem, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	if (licenseSnapshots.snapshotLength > 0) {
+		this.license = {
+			text: licenseSnapshots.snapshotItem(0).innerText.trim(),
+			link: licenseSnapshots.snapshotItem(0).getAttribute('href')
+		}
+	}
+
+	// principles, from hNews, recommended
+	var principleSnapshots = document.evaluate("//a[@rel='principles']", hNewsElem, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	if (principleSnapshots.snapshotLength > 0) {
+		this.principle = {
+			text: principleSnapshots.snapshotItem(0).innerText.trim(),
+			link: principleSnapshots.snapshotItem(0).getAttribute('href')
+		}
+	}
+
+	// dateline, from hNews, optional
+	// Note: not supporting the hCard implementation
+	var datelines = hNewsElem.getElementsByClassName('dateline');
+	if (datelines.length > 0) {
+		this.dateline = datelines[0].innerText.trim();
+	}
+
+	// geo, from hNews, optional
+	var geoLocations = hNewsElem.getElementsByClassName('geo');
+	if (geoLocations.length > 0) {
+		var latitudes = geoLocations[0].getElementsByClassName('latitude');
+		var longitudes = geoLocations[0].getElementsByClassName('longitude');
+		if (latitudes.length > 0 && longitudes.length > 0) {
+			// Note we have alternate handlings here
+			this.geo = {
+				latitude: Number((latitudes[0].getAttribute('title')) ? latitudes[0].getAttribute('title') : latitudes[0].innerText),
+				longitude: Number((longitudes[0].getAttribute('title')) ? longitudes[0].getAttribute('title') : longitudes[0].innerText)
+			}
+		}
+	}
+
+	// published, from hAtom, optional
+	var publishedDates = hNewsElem.getElementsByClassName('published');
+	if (publishedDates.length > 0) {
+		this.published = Date.parse(publishedDates[0].getAttribute('title'));
+	}
+
+	// finally, set something telling folks we're valid hNews or not.
+	this.isValid = (this.title && this.author && this.updated && this.source) ? true : false;
+};
+
+if(typeof(String.prototype.trim) === "undefined") {
+	String.prototype.trim = function() {
+		return String(this).replace(/^\s+|\s+$/g, '');
+	};
+}
 
 /**
  * Checks to see if an hnews container exists, sends a message to Chrome if it
@@ -6,60 +95,21 @@
 function CheckForHNews () {
 	var hNewsContainer = document.getElementsByClassName('hnews hentry');
 	if (hNewsContainer.length > 0) {
-		// Ok, we have an hNews container... do something...
-		// FIXME if there's more than one?  Can there be?
+		var hnews = new hNews(hNewsContainer[0]);
+		console.log(hnews);
 
-		var hEntryData = navigator.microformats.get('hEntry', hNewsContainer[0].parentNode);
-		console.log(hEntryData);
-
-		// Note that the source-org requirement is outside the hAtom spec so we
-		// have to deal with it separately
-		var source_orgData = {};
-		var source_orgContainer = hNewsContainer[0].getElementsByClassName('source-org');
-		if (source_orgContainer.length > 0) {
-			// Fool microformat-shiv into taking a node that's a microformat
-			var tempDiv = document.createElement('div');
-			tempDiv.appendChild(source_orgContainer[0].cloneNode(true));
-
-			source_orgData = navigator.microformats.get('hCard', tempDiv);
-			console.log(source_orgData);
-		}
-
-		// hNews geo
-		var geoData = {};
-		var geoContainer = hNewsContainer[0].getElementsByClassName('geo');
-		if (geoContainer.length > 0) {
-			// Fool microformat-shiv into taking a node that's a microformat
-			var tempDiv = document.createElement('div');
-			tempDiv.appendChild(geoContainer[0].cloneNode(true));
-
-			geoData = navigator.microformats.get('geo', tempDiv);
-			console.log(geoData);
-		}
-
-		// AP/hNews item-license
-		var licenseData = {};
-		var licenseXpath = document.evaluate("//a[@rel='item-license']", hNewsContainer[0], null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-		if (licenseXpath.snapshotLength > 0) {
-			licenseData = {
-				text: licenseXpath.snapshotItem(0).innerHTML,
-				link: licenseXpath.snapshotItem(0).getAttribute('href')
+		// AP's News Registry beacon
+		var beacon = {}
+		var beaconSnapshots = document.evaluate("//img[contains(@src, 'apnewsregistry')]", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+		if (beaconSnapshots.snapshotLength > 0) {
+			beacon = {
+				link: beaconSnapshots.snapshotItem(0).getAttribute('src')
 			}
-			console.log(licenseData);
-		}
-
-		// AP's beacon
-		var beaconData = {}
-		var beacon = document.evaluate("//img[contains(@src, 'apnewsregistry')]", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-		if (beacon.snapshotLength > 0) {
-			beaconData = {
-				link: beacon.snapshotItem(0).getAttribute('src')
-			}
-			console.log(beaconData);
+			console.log(beacon);
 		}
 
 		// Send the data along...
-		chrome.extension.sendRequest({hEntry: hEntryData, hCard: source_orgData, geo: geoData, itemLicense: licenseData, beacon: beaconData});
+		chrome.extension.sendRequest({hNews: hnews, beacon: beacon});
 	}
 }
 
